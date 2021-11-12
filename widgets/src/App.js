ed3ea18ecdcd060426166119a8ba9a96f9ba0aee
Logan Murray <lpmurray16@users.noreply.github.com>
2021-11-12 16:17:15 -0600

    added global.css and updated package.json

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,10 +3,9 @@ import Accordion from './components/Accordion';
 import items from './components/items';
 
 
-
 export default () => {
     return (
-        <div>
+        <div className = "accordion">
             <h1>Harry Potter Trivia</h1>
             <Accordion items={items}/>
         </div>
