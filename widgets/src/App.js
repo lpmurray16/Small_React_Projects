bd95ec70dc27cc00b2192dc6e809d94170aab488
Logan Murray <lpmurray16@users.noreply.github.com>
2021-11-12 21:52:40 -0600

    added some styling and an image

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -6,7 +6,7 @@ import items from './components/items';
 export default () => {
     return (
         <div className = "accordion">
-            <h1>Harry Potter Trivia</h1>
+            <h1 className = "title_top"><img src="harrypotter_title.png" alt="something"/>Trivia</h1>
             <Accordion items={items}/>
         </div>
     );
