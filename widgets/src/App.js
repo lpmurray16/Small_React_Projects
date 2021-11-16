0c99366775505af0df56b07e1e3951e1868605d1
Logan Murray <lpmurray16@users.noreply.github.com>
2021-11-15 22:07:05 -0600

    done with search widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -6,7 +6,7 @@ import Search from './components/Search';
 
 export default () => {
     return (
-        <div className = "accordion">
+        <div className = "accordion wider">
             {/* <h1 className = "title_top"><img src="harrypotter_title.png" alt="something"/>Trivia</h1> */}
             {/* <Accordion items={items}/> */}
             <Search/>
