84ab350c238552abc49e26745c3e7afcc5de6bb7
Logan Murray <lpmurray16@users.noreply.github.com>
2021-11-15 22:06:46 -0600

    doen with search widget

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -4,12 +4,13 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
-    cosnt [results, setResult] = useState('<p>Start typing search</p>');
+    const [results, setResults] = useState([]);
     
+    console.log(results[0]);
     
     useEffect(() => {
         const searchWiki = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
@@ -18,19 +19,69 @@ const Search = () => {
                     srsearch: term,
                 },
             });
+
+            setResults(data.query.search);
         };
 
-        searchWiki();
+        // THIS ISN'T NEEDED FOR MY IMPLEMENTATION
+        // if(term && !results.length) {
+        //     searchWiki();
+        // } else {
+        //     const timeoutId = setTimeout(() => {
+        //         if(term) {
+        //             searchWiki();
+        //         }
+        //     }, 500);
+            
+        //     return( () => {
+        //         clearTimeout(timeoutId);
+        //     });
+        // }
+
+        const timeoutId = setTimeout(() => {
+            if(term) {
+                searchWiki();
+            }
+        }, 500);
+        
+        return( () => {
+            clearTimeout(timeoutId);
+        });
+
     }, [term]);
 
-    return (
-        <div>
-            <div className = "ui form">
-                <div className = "field">
-                    <label>Enter Search Term</label>
-                    <input className = "input" value= {term} onChange={(e) => setTerm(e.target.value)}/>
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key = { result.pageid } className = "item extra-room">
+                <div className = "right floated content">
+                    <a href={`https://en.wikipedia.org?curid=${ result.pageid }`} target="_blank" rel="noopener noreferrer" className = "ui primary button">Go</a>
+                </div>
+                <div className = "content">
+                    <a href={`https://en.wikipedia.org?curid=${ result.pageid }`} 
+                    target="_blank"
+                    rel="noopener noreferrer">
+                        <div className = "header color-blue">{ result.title }</div>
+                    </a>
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}></span><span>. . . </span>
+                    <p>----- Click GO to read more -----</p>
                 </div>
             </div>
+        );
+    });
+    return (
+        <div className="wider">
+            <div className = "ui form">
+                <div className = "field">
+                    <label className = "color-white">Enter Search Term</label>
+                    <input className = "input" placeholder="Start Typing..." value= {term} onChange={(e) => setTerm(e.target.value)}/>
+                </div>
+            </div>
+
+
+            <div className = "ui celled list bg-white">{ renderedResults }</div>
+
+        
         </div>
     );
 };
