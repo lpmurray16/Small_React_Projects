5fd56d1ecaa5cec8462799cb11bb3651bd10c038
Logan Murray <lpmurray16@users.noreply.github.com>
2021-11-22 22:26:02 -0600

    pretty much finished dropdown component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import items from './components/items';
 import Search from './components/Search';
 
 
+const options = [
+    {
+        label: 'The color red',
+        value: 'red'
+    },
+    {
+        label: 'The color green',
+        value: 'green'
+    },
+    {
+        label: 'The color blue',
+        value: 'blue'
+    }
+];
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div className = "accordion wider">
             {/* <h1 className = "title_top"><img src="harrypotter_title.png" alt="something"/>Trivia</h1> */}
             {/* <Accordion items={items}/> */}
-            <Search/>
+            {/* <Search/> */}
+            <Dropdown 
+                selected={selected}
+                onSelectedChange={setSelected} 
+                options= { options }
+            />
         </div>
     );
 };
