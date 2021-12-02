4527e3d82759083591c97b33116e928546e4fd98
Logan Murray <lpmurray16@users.noreply.github.com>
2021-12-01 23:53:45 -0600

    in the middle of setting up routing

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,48 +1,98 @@
-import React, {useState} from 'react';
-import Accordion from './components/Accordion';
-import Dropdown from './components/Dropdown';
-import items from './components/items';
-import Search from './components/Search';
-import Translate from './components/Translate';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
+import items from "./components/items";
+import Search from "./components/Search";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 
 const options = [
-    {
-        label: 'The color red',
-        value: 'red'
-    },
-    {
-        label: 'The color green',
-        value: 'green'
-    },
-    {
-        label: 'The color blue',
-        value: 'blue'
-    }
+  {
+    label: "The color red",
+    value: "red",
+  },
+  {
+    label: "The color green",
+    value: "green",
+  },
+  {
+    label: "The color blue",
+    value: "blue",
+  },
 ];
 
-export default () => {
-    // const [selected, setSelected] = useState(options[0]);
-    // const [showDropdown, setShowDropdown] = useState(true);
+// {{{{{{{ UNUSED, very basic way of implementing routing }}}}}}}
+// const showAccordion = () => {
+//   if (window.location.pathname === "/") {
+//     return <Accordion items={items} />;
+//   }
+// };
+// const showSearch = () => {
+//   if (window.location.pathname === "/search") {
+//     return <Search />;
+//   }
+// };
+// const showTranslate = () => {
+//   if (window.location.pathname === "/translate") {
+//     return <Translate />;
+//   }
+// };
+// const showDropdown = () => {
+//   if (window.location.pathname === "/dropdown") {
+//     return <Dropdown />;
+//   }
+// };
 
-    return (
-        <div className = "accordion wider flex-center col">
-            {/* <h1 className = "title_top"><img src="harrypotter_title.png" alt="something"/>Trivia</h1> */}
-            {/* <Accordion items={items}/> */}
-            {/* <Search/> */}
-            {/* <button className = "ui button" onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-            {showDropdown ? (
-                <div>
-                <Dropdown 
-                    selected={selected}
-                    onSelectedChange={setSelected} 
-                    options= { options }
-                />
-                <p className="dropdown-text" style={{color: selected.value}}>The text is {selected.value}</p>
-                </div>
-                ) : null
-            } */}
-            <Translate />
-        </div>
-    );
+// {{{{ Similar to the above functions, but still not very React-oriented }}}}
+// const showComponent = (route, component) => {
+//     return window.location.pathname === route
+//     ? component
+//     : null;
+// };
+
+export default () => {
+  // both of these state variables are used in Dropdown
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+  
+  // this is our state variable to handle routing
+  const [path, setPath] = useState("/");
+
+  // final JSX returned from App.js which includes and uses all components within this application.
+  return (
+    <div className="accordion wider flex-center col">
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/dropdown">
+        <button
+          className="ui button"
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          Toggle Dropdown
+        </button>
+        {showDropdown ? (
+          <div>
+            <Dropdown
+              selected={selected}
+              onSelectedChange={setSelected}
+              options={options}
+            />
+            <p className="dropdown-text" style={{ color: selected.value }}>
+              The text is {selected.value}
+            </p>
+          </div>
+        ) : null}
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
 };
--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -21,7 +21,7 @@ const Accordion = ({ items }) => {
 
         const active = index === activeIndex ? 'active':'';
         return (
-        
+        // Using React.Fragment in order to wrap these two divs together without adding an extra node to the DOM
         <React.Fragment key={item.title}> 
             {/* sets the class to active or empty string based on whether or not the index value is the activeIndex value from the state object */}
             {/* onTitleClick which is created above just sets this item from the array as the active item and sets its index as the activeIndex */}
@@ -41,9 +41,13 @@ const Accordion = ({ items }) => {
 
     // final returned JSX div for Accordion component, uses renderedItems from above.
     return (
-        <div className="ui styled accordion">
-            {renderedItems}
+        <div>
+            <h1 className = "title_top"><img src="harrypotter_title.png" alt="Harry Potter Movie Title Graphic"/>Trivia</h1>
+            <div className="ui styled accordion">
+                {renderedItems}
+            </div>
         </div>
+        
     );
 };
 
