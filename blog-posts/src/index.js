c6fcd68881077c230b83ec66181e34582ef58799
Logan Murray <lpmurray16@users.noreply.github.com>
2022-06-27 14:56:50 -0500

    working on it

--- blog-posts/src/index.js
+++ blog-posts/src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 // Import the main App component and the reducers
 import App from "./components/App";
 import reducers from "./reducers";
 
 const root = createRoot(document.getElementById("root"));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 root.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>
 );
