2c54df84f2f776c523711c91830fecde0527d4e2
Logan Murray <lpmurray16@users.noreply.github.com>
2022-06-29 15:31:49 -0500

    about to optimize for less user calls w memo

--- blog-posts/src/components/PostList.js
+++ blog-posts/src/components/PostList.js
@@ -2,14 +2,39 @@ import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 
+import UserHeader from "./UserHeader";
+
 class PostList extends React.Component {
     componentDidMount() {
         this.props.fetchPosts();
     }
 
+    renderList() {
+        return this.props.posts.map((post) => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h3 className="post__title">{post.title}</h3>
+                            <p className="post__description">{post.body}</p>
+                            <UserHeader userId={post.userId} />
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
-        return <div className="ui container">Post List</div>;
+        return (
+            <div className="ui relaxed divided list">{this.renderList()}</div>
+        );
     }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+    return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
